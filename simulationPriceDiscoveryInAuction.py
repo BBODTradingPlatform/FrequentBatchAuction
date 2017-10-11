c9cb1c98d529ac2cf45c9ec4c47e101978bbeb72
BlockChain Board of Derivatives <32043632+BBODTradingPlatform@users.noreply.github.com>
2017-10-11 15:28:28 +0200

    Rearrangement of the code

--- simulationPriceDiscoveryInAuction.py
+++ simulationPriceDiscoveryInAuction.py
@@ -22,97 +22,103 @@ import numpy as np
 import random
 import sys
 
+# function of auctionPrice calculation
+def calculateAuctionPrice(priceVolumeTemp):
+    sum = {}
+    auctionPrice = {}
+    for side in ["sell", "buy"]:
+
+        # here we weight price with volume
+        priceVolumeTemp[side]["priceVolume"] = np.multiply(priceVolumeTemp[side]["price"], priceVolumeTemp[side]["volume"])
+        sum[side] = {"priceVolume": np.sum(priceVolumeTemp[side]["priceVolume"]), "volume": np.sum(priceVolumeTemp[side]["volume"])}
+
+        # actionPrice is the weighted price with volume for sell and buy separetly
+        auctionPrice[side] = sum[side]["priceVolume"]/sum[side]["volume"] if sum[side]["volume"] > 0 else 0 
+
+        # We take average of weighted sell and buy price, also  it is returned volume generated in auction as the min value of sell and buy side
+    return (auctionPrice["sell"] + auctionPrice["buy"])/2.0, min(sum["sell"]["volume"], sum["buy"]["volume"])
+
+#here we initiate the global parameters
 stablePriceCount = 0
-stableVolumeCount = 0
-totalPriceCount = 0
-totalVolumeCount = 0
-finalAuctionpriceList = []
-finalnoPartiesList = []
-totalnoPartiesList = []
+auctionVolume = 0
+totalVolume = 0
+
+# parameter describes how large random numbers will be, so max = 2 ** noOfBitsInRandom
+noOfBitsInRandom = 10
 totalRounds = 1000  # here you can set how many rounds of MonteCralo you would like to run
 for z in range(totalRounds):
+
+    #just to show the progress
     if z%100 == 0:
-        print("%f %% was done" % (z/totalRounds * 100))
-    totalPartiesSELL = random.getrandbits(8) # this function gives random integers between 0 and 255, here is total number of SELLers in Auction
-    totalPartiesBUY = random.getrandbits(8) # total number of BUYers in auction
+        print("%.0f %% was done" % (z/totalRounds * 100))
+
+    # this function gives random integers between 0 and 2 ** noOfBitsInRandom, here are total number of Sellers and Buyers in Auction
+    totalNoUsers = {"sell": random.getrandbits(noOfBitsInRandom), "buy": random.getrandbits(noOfBitsInRandom)} 
     
-    listSELL = []
-    for i in range(totalPartiesSELL):
-        listSELL.append([random.getrandbits(8), random.getrandbits(8)]) # first number is the SELL price and second in list is just volume to SELL price all ranges randomly from 0 to 255
-    listBUY = []
+    # here we generate random values (limit price and volume) for each user in Auction
+    priceVolume = {}
+    for side in ["sell", "buy"]:
+        priceVolume[side] = {"price": [random.getrandbits(noOfBitsInRandom) for i in range(totalNoUsers[side])], 
+                                "volume": [random.getrandbits(noOfBitsInRandom) for i in range(totalNoUsers[side])]}
 
-    for i in range(totalPartiesBUY):
-        listBUY.append([random.getrandbits(8), random.getrandbits(8)]) # first number is the BUY price and second in list is just volume to BUY price
-   
-    sumSELL = 0
-    sumBUY = 0
-    volumeBUY = 0
-    volumeSELL = 0
-    for i in range(len(listSELL)):
-        sumSELL += listSELL[i][0] * listSELL[i][1] # weighted (by volume) sum of SELL price
-        volumeSELL += listSELL[i][1] # sum of volume on SELL side
+        #we take into account both sides, so we have to divide by factor of 2
+        totalVolume += np.sum(priceVolume[side]["volume"])/2.0
+    
+    # we initaite inital value of auctionPrice for the porpose if in the loop auctionPrice is changing
+    previousAuctionPrice = 0
 
-    for i in range(len(listBUY)):
-        sumBUY += listBUY[i][0] * listBUY[i][1] #weighted (by volume) sum of BUY price
-        volumeBUY += listBUY[i][1] # sum of volume on BUY side
+    #we calculate initAuctionPrice and volume taken place in this auction
+    currentAuctionPrice, currentAuctionVolume = calculateAuctionPrice(priceVolume.copy())
+    priceVolumeTemp = {}
 
-    totalVolumeCount += volumeBUY + volumeSELL # sum of all sums of volume SELL and BUY side only just for use in statistics
-    totalPriceCount += 1 # in the end will be just totalRounds
+    # self-consistent method main loop
+    for no in range(100): 
+        side = "sell"
+        priceVolumeTemp[side] = {}
+        priceAccepted = np.array([currentAuctionPrice>priceVolume[side]["price"]])
 
-    if volumeBUY > 0 and volumeSELL > 0: # just to check that we not divide by zero
-        priceAuctionInit = (sumBUY/volumeBUY + sumSELL/volumeSELL) / 2.0 #calculate of initial price (see AuctionPrice at introduction on beginning of file)
-    else:
-        priceAuctionInit = 0
-    priceCurrent = 0
-    for q in range(100): # self-consistent method main loop
-        if priceCurrent == 0:
-            priceCurrent = priceAuctionInit # just initialization of price
-        else:
-            priceAuctionInit = priceCurrent
-        sumSELL = 0
-        sumBUY = 0
-        volumeBUY = 0
-        volumeSELL = 0
-        noPartiesSELL = 0
-        noPartiesBUY = 0
-        for i in range(len(listBUY)):
-            if priceCurrent < listBUY[i][0]: # just check if buyers limit price is above auction price, so then he is taker, in other case he is maker
-                sumBUY += listBUY[i][0] * listBUY[i][1]
-                volumeBUY += listBUY[i][1]
-                noPartiesBUY += 1
-        for i in range(len(listSELL)):
-            if priceCurrent > listSELL[i][0]: # just check if seller limit price is below auction price, so then he is taker, in other case he is maker
-                sumSELL += listSELL[i][0] * listSELL[i][1]
-                volumeSELL += listSELL[i][1]
-                noPartiesSELL += 1
-
+        # we prune offer prices of Makers (which should not be counted in auctionPrice). Only takers determine auctionPrice
+        for type in ["price", "volume"]:
+            if np.any(priceAccepted == True):
+                priceVolumeTemp[side][type] = [priceVolume[side][type][i] for i in range(totalNoUsers[side]) if currentAuctionPrice > priceVolume[side]["price"][i]]
         
-        if volumeBUY > 0 and volumeSELL > 0:
-            priceCurrent = (sumBUY/volumeBUY + sumSELL/volumeSELL) / 2.0 # temporary AuctionPrice in self-consistent method loop
-        else:
-            priceCurrent = 0;
-            priceAuctionInit = 0
-            break # if there is no buyers or no sellers just self-consistent method fail to find stable price
-        if priceAuctionInit == priceCurrent:
-            break # if the price was not further changed in loop, so get out of loop. Hurra, we have stable AuctionPrice !!!
+        side = "buy"
+        priceVolumeTemp[side] = {}
+        priceAccepted = np.array([currentAuctionPrice<priceVolume[side]["price"]])
 
-       # print("Current auction price: %f with volumeBUY = %f and volumeSell = %f" % (xpricecurrent, volumeBUY, volumeSELL))
-    #print("no of SELL clients = %f [%%], no of BUY clients = %f [%%]" % (noPartiesSELL/totalPartiesSELL * 100, noPartiesBUY/totalPartiesBUY * 100))
-    #sys.stdin.read(1)
-    if priceCurrent > 0 and q < 100: # if AuctionPrice is above zero (so we found stable price). Also we check if the loop finished before reaching 100, means we truely find stable price. if q == 100, means the price is unstable or perdiodicaly changed.
+        # we prune offer prices of Makers (which should not be counted in auctionPrice). Only takers determine auctionPrice
+        for type in ["price", "volume"]:
+            if np.any(priceAccepted == True):
+                priceVolumeTemp[side][type] = [priceVolume[side][type][i] for i in range(totalNoUsers[side]) if currentAuctionPrice < priceVolume[side]["price"][i]]
+        
+        # here it is checked if there is both sell and buy side 
+        if len(priceVolumeTemp["sell"]) + len(priceVolumeTemp["buy"]) == 4:
+            currentAuctionPrice, currentAuctionVolume = calculateAuctionPrice(priceVolumeTemp.copy())
+        else:
+
+            # we break loop if it is only sellers or buyers
+            currentAuctionPrice = 0
+            break
+
+        # we break loops becuase auctionPrice is not stable and does not gives correct value
+        if currentAuctionPrice <= 0:
+            break 
+
+        # if the price was not further changed in loop, so get out of loop. Hurra, we have stable AuctionPrice !!!
+        if currentAuctionPrice == previousAuctionPrice and previousAuctionPrice > 0:
+            break 
+
+        previousAuctionPrice = currentAuctionPrice
+        #print("Final auction price: %f with volume = %f" % (currentAuctionPrice, currentAuctionVolume))
+
+    # if AuctionPrice is above zero (so we found stable price). 
+    # Also we check if the loop finished before reaching 100, means we truely find stable price. 
+    # if q == 100, means the price is unstable or perdiodicaly changed.
+    if currentAuctionPrice > 0 and no < 100:
         stablePriceCount += 1
-        finalnoPartiesList.append(noPartiesSELL/totalPartiesSELL * 50 + noPartiesBUY/totalPartiesBUY * 50)
-        totalnoPartiesList.append(totalPartiesSELL + totalPartiesBUY )
-        finalAuctionpriceList.append(priceCurrent)
-        if volumeSELL > volumeBUY:
-            stableVolumeCount += volumeBUY
-        else:
-            stableVolumeCount += volumeSELL
-
-  #  print("Final auction price: %f with volumeBUY = %f and volumeSell = %f" % (xpricecurrent, volumeBUY, volumeSELL))
-print(finalAuctionpriceList) # list of all final stable prices in Auction
-print("Stable Price = %f [%%], Stable Volume = %f [%%]" % (stablePriceCount/totalPriceCount * 100, stableVolumeCount/totalVolumeCount * 100))
-print("Average Price = %f, SD of price = %f" % (np.mean(finalAuctionpriceList), np.std(finalAuctionpriceList)))  
-print("Average no of takers = %f [%%], SD of no of takers = %f [%%]" % (np.mean(finalnoPartiesList), np.std(finalnoPartiesList)))
-print("Average no of clients = %f, SD of no of clients = %f" % (np.mean(totalnoPartiesList), np.std(totalnoPartiesList)))
+        auctionVolume += currentAuctionVolume
+    
+#we print out statistics from MonteCarlo simulation
+print("Percent of Stable Auction Price = %.2f %%, Percent of Stable Auction Volume = %.2f %%" % (stablePriceCount/totalRounds * 100, auctionVolume/totalVolume * 100))
 sys.stdin.read(1)
+
